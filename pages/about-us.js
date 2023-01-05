8d270d99e0be8baf094bc1266090020c6d89a8be
Do Tuan Dat <dathymax@gmail.com>
2023-01-05 20:46:41 +0700

    update

---
+++ pages/about-us.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Page = () => {
+	return (
+		<div>
+			<h1>About us page</h1>
+			<p>
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+				Accusamus expedita officiis magni! Porro impedit qui distinctio
+				reiciendis consequatur rem. Repellendus molestiae hic laborum
+				quis sit unde repellat id a iste?
+			</p>
+		</div>
+	);
+};
+
+export default Page;
---
+++ pages/events/[catergories]/[id].js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Page = () => {
+	return <div>Event in London</div>;
+};
+
+export default Page;
