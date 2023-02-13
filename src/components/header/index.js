75fff2edf5c4cbe8e1a9f6f552387e4d3176ccf4
Do Tuan Dat <dathymax@gmail.com>
2023-02-13 22:38:20 +0700

    update

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,21 +1,31 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 
 const Header = () => {
 	return (
 		<header>
-			<nav>
-				<img />
-				<Link href="/" passHref>
-					Home
-				</Link>
-				<Link href="/events" passHref>
-					Events
-				</Link>
-				<Link href="/about-us" passHref>
-					About us
-				</Link>
-			</nav>
+			<div>
+				<Image
+					src={"/images/logo.png"}
+					width={50}
+					height={50}
+					alt={"logo"}
+				/>
+				<nav>
+					<img />
+					<Link href="/" passHref>
+						Home
+					</Link>
+					<Link href="/events" passHref>
+						Events
+					</Link>
+					<Link href="/about-us" passHref>
+						About us
+					</Link>
+				</nav>
+			</div>
+			<h1>Lorem ipsum dolor sit amet.</h1>
 		</header>
 	);
 };
